db0a245955350144d8897bba17c798c524017354
kentakazumi <majorkzmkkzmk@gmail.com>
2021-04-09 11:28:46 +0000

    apri2-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#top'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  # 以下の行のonly: []内にeditを追加
+  resources :users, only: [:show, :edit, :update]
 end
