4d74d74335987d43b4d6536a7c68611880b7b13d
kentakazumi <majorkzmkkzmk@gmail.com>
2021-04-09 03:32:20 +0000

    Second Comit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3587dc827d68e8fc758bbbd697a2841bc9c10c9cc3af84c4a375aa3712f49070029106f6b158bdd8d89adf85aa63a41d5c91010b662f27943803cd1ee23f4953'
